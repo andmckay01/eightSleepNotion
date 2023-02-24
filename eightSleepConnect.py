0c1f59752d68c802e2a8da7fa02b904c81c9d1f9
Mckay Anderson <andmckay01@gmail.com>
2023-02-24 08:08:57 -0700

    removed notion code from eightSleepConnect.py

--- eightSleepConnect.py
+++ eightSleepConnect.py
@@ -1,11 +1,1 @@
-import json
-import os
-import requests as req
-from dotenv import load_dotenv
-
-path = "/Users/mckayanderson/Documents/catacombs/devInTraining/eighSleepNotion"
-
-load_dotenv()
-notionToken = os.getenv('notionToken')
-token = notionToken
-database_id = '23b87a29080f4644bf3c0680da72dc01'
+import pyEight
